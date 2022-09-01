ca83c9a856dcc4f65336ea43492c507587f8aae1
thetejasmagade <53045273+thetejasmagade@users.noreply.github.com>
2022-09-01 23:29:57 +0530

    Initial Commit

---
+++ components/Default.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <Sidebar v-if="!$device.isMobile"/>
+    <MobileSidebar v-else />
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Default />
+    <v-main>
+      <Navbar v-if="!$device.isMobile"/>
+      Java, vuesdvsvsvd
+    </v-main>
+  </div>
+</template>
+
+<script>
+import MobileSidebar from '~/components/MobileSidebar.vue';
+import Default from '~/components/Default.vue';
+export default { components: { MobileSidebar, Default } };
+</script>
+
+<style>
+</style>
